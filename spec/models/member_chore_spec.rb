8b85a1ad9e3e4ccd31866b4914aa12df71433a31
Adam Bailey <ab67319@gmail.com>
2023-05-12 13:52:41 -0600

    test: create model relationship and validation tests
    Co-authored-by: bphayes1200 <brian.hayes1200@gmail.com>
    Co-authored-by: hamouj <jasmine.mahabadi@gmail.com>
    Co-authored-by: MAtrevino <meredithtrevino3@gmail.com>
    Co-authored-by: MoxieMax <mary.macgillivray@gmail.com>

--- spec/models/member_chore_spec.rb
+++ spec/models/member_chore_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe MemberChore, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should belong_to :member }
+    it { should belong_to :chore }
+  end
 end
--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Member, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should belong_to :household }
+    it { should have_many :member_chores }
+  end
 end

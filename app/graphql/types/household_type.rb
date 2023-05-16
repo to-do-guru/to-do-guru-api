60f43a90d57a76ccad63acc5feebd07355bc5bff
Meredith Trevino <meredithtrevino3@gmail.com>
2023-05-16 11:08:44 -0600

    adds a query to get all chores for a household
    Co-authored-by: ab67319 <ab67319@gmail.com>
    Co-authored-by: bphayes1200 <brian.hayes1200@gmail.com>
    Co-authored-by: hamouj <jasmine.mahabadi@gmail.com>
    Co-authored-by: MoxieMax <mary.macgillivray@gmail.com>

--- app/graphql/types/household_type.rb
+++ app/graphql/types/household_type.rb
@@ -7,5 +7,57 @@ module Types
     field :email, String
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
-  end
+    field :chores, [Types::ChoreType], null: true
+    field :members, [Types::MemberType], null: true
+    field :sunday, [Types::ChoreType], null: true
+    field :monday, [Types::ChoreType], null: true
+    field :tuesday, [Types::ChoreType], null: true
+    field :wednesday, [Types::ChoreType], null: true
+    field :thursday, [Types::ChoreType], null: true
+    field :friday, [Types::ChoreType], null: true
+    field :saturday, [Types::ChoreType], null: true
+    
+    def sunday
+      sunday = object.chores.where(day: "Sunday")
+      format_day(sunday)
+    end
+
+    def monday
+      monday = object.chores.where(day: "Monday")
+      format_day(monday)
+    end
+
+    def tuesday
+      tuesday = object.chores.where(day: "Tuesday")
+      format_day(tuesday)
+    end
+
+    def wednesday
+      wednesday = object.chores.where(day: "Wednesday")
+      format_day(wednesday)
+    end
+
+    def thursday
+      thursday = object.chores.where(day: "Thursday")
+      format_day(thursday)
+    end
+
+    def friday
+      friday = object.chores.where(day: "Friday")
+      format_day(friday)
+    end
+
+    def saturday
+      saturday = object.chores.where(day: "Saturday")
+      format_day(saturday)
+    end
+
+    def format_day(day)
+      if day.empty?
+        return nil
+      else
+        return day
+      end
+    end
+  end  
 end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,17 +1,13 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add `node(id: ID!) and `nodes(ids: [ID!]!)`
     include GraphQL::Types::Relay::HasNodeField
     include GraphQL::Types::Relay::HasNodesField
+    field :household, Types::HouseholdType, null: false, description: "returns one household" do
+      argument :email, String, required: true
+    end
 
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
-
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    def household(email:)
+      Household.find_by(email: email)
     end
   end
 end

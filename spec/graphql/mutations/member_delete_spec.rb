6a41d8cabf12824feba910b364e7981496092376
Max MacGillivray <mary.macgillivray@gmail.com>
2023-05-17 16:24:07 -0700

    First test to ensure all household members are created for testing purposes is passing.

--- spec/graphql/mutations/member_delete_spec.rb
+++ spec/graphql/mutations/member_delete_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Mutations::DeleteMember, type: :graphql do
+RSpec.describe Mutations::MemberDelete, type: :graphql do
   describe '#resolve' do
     let!(:household) { create(:household) }
     let!(:member1) { create(:member, household: household) }
@@ -8,7 +8,7 @@ RSpec.describe Mutations::DeleteMember, type: :graphql do
     let!(:member3) { create(:member, household: household) }
     
     it 'starts with three household members for testing purposes' do
-      
+      expect(household.members.count).to eq(3)
     end
   end
 end

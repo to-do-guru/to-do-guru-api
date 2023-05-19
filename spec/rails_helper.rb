0db7976818bc2462a1c05581ed6dad0cfeacb5e3
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-05-19 09:13:31 -0700

    chore: add filter for SimpleCov

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,9 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require "simplecov"
-SimpleCov.start
+SimpleCov.start do
+  add_filter "app/controllers/graphql_controller.rb"
+  add_filter "app/graphql/to_do_guru_api_schema.rb"
+end
 require "spec_helper"
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"

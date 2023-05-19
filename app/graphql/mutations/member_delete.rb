187ac13c8c904b8cb8ce713b7e543139e1f7bdfb
Max MacGillivray <mary.macgillivray@gmail.com>
2023-05-19 10:10:05 -0700

    Lint: update formatting based on RuboCop suggestions.”

--- app/graphql/mutations/member_delete.rb
+++ app/graphql/mutations/member_delete.rb
@@ -3,17 +3,17 @@
 module Mutations
   class MemberDelete < BaseMutation
     description "Deletes a member by ID"
-    
+
     argument :id, ID, required: true
 
     field :member, Types::MemberType, null: true
     field :errors, [String], null: false
-    
-    def resolve(id:, **attributes)
+
+    def resolve(id:, **_attributes)
       member = Member.find(id)
-      
+
       member.destroy
-      
+
       { member:, errors: [] }
     rescue StandardError => e
       { errors: [e] }
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -65,7 +65,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-  
+
   # config.include RSpec::GraphqlMatchers::Matchers
 
 end

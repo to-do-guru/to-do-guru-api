cb33babe22c09f6bf882f94283075c4700bfcdbc
Meredith Trevino <meredithtrevino3@gmail.com>
2023-05-19 17:12:49 -0400

    adds model test for presence of name for member

--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -5,5 +5,6 @@ RSpec.describe Member, type: :model do
     it { should belong_to :household }
     it { should have_many :member_chores }
     it { validate_uniqueness_of :name }
+    it { validate_presence_of :name}
   end
 end

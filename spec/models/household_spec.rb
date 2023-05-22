9f9f8b2e1c3ccfa132fafd1ecb78a25a36e34fdd
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-05-22 09:13:16 -0700

    test: create test for randomize_chores method

--- spec/models/household_spec.rb
+++ spec/models/household_spec.rb
@@ -5,4 +5,24 @@ RSpec.describe Household, type: :model do
     it { should have_many :chores }
     it { should have_many :members }
   end
+
+  context 'instance methods' do
+    describe '#randomize_chores' do
+      let!(:household) { create(:household) }
+      let!(:member1) { create(:member, household: household) }
+      let!(:member2) { create(:member, household: household) }
+      let!(:chore1) { create(:chore, household: household, duration: 30) }
+      let!(:chore2) { create(:chore, household: household, duration: 45) }
+      let!(:chore3) { create(:chore, household: household, duration: 60) }
+      let!(:chore4) { create(:chore, household: household, duration: 30) }
+
+      it 'creates memberchores to evenly distribute chores' do
+        household.randomize_chores
+
+        # distribute as evenly as possible
+        expect(member1.chores).to eq([chore1, chore2])
+        expect(member2.chores).to eq([chore3, chore4])
+      end
+    end
+  end
 end

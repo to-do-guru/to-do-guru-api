e69d52f4b9e7a8e10349a474a0b1ab60b9564e44
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-05-22 09:13:38 -0700

    feat: create beginning of randomize_chores method

--- app/models/household.rb
+++ app/models/household.rb
@@ -2,4 +2,28 @@
 class Household < ApplicationRecord
   has_many :members, dependent: :destroy
   has_many :chores, dependent: :destroy
+
+  def randomize_chores
+    chores = self.chores.select(:id, :duration)
+    # num_chores = chores.size
+    total_duration = self.chores.sum(:duration)
+    members = self.members
+    # num_members = members.size
+
+    average_duration_per_member = total_duration.to_f / members.size
+
+    assignments = {}
+    accumulated_durations = Hash.new(0)
+
+    chores.each do |chore|
+      members.each do |member|
+        if accumulated_durations[member] + chore[:duration] <= average_duration_per_member
+          assignments[chore.id] = member
+          accumulated_durations[member] += chore[:duration]
+          break
+        end
+      end
+    end
+   require 'pry'; binding.pry
+  end
 end
